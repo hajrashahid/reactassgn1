412d3128290656732e249d3d0e55fc372c47b1e7
hajrashahid <hajrashahid92@gmail.com>
2020-06-10 05:35:37 +0500

    commit
    cmmthfgh

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Hello.css';
+function App() {
+  return (
+    
+  <div>Hello <strong className="myname">Hajra Shahid</strong>!
+      <div>
+        <h1>This is my first heading </h1>
+        <ol> <b>Countries affected by COVID-19</b>
+         <li>Italy</li>
+         <li>Spain</li>
+         <li>America</li>
+         <li>UK</li>
+         <li>Pakistan</li>
+        </ol>
+        <ul>Now starts an unordered list</ul>
+        <li>China</li>
+        <li>Russia</li>
+        <li>Afghanistan</li>
+        <li>India</li><br/>
+               <div>
+                This is an example of JS expressions. 5+9={5+9} 
+               </div>
+
+
+      </div>
+      
+  </div>
+  )
+}
+
+export default App;
